326907a32e1b8efa194fdd6fb7f12082051b55db
Lyndon Joseph <lyndon@Lyndons-MacBook-Pro.local>
2019-11-13 13:11:35 -0500

    yo gangs

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// var or const 
+let itemsInCart =0;
+
+function addAnItemToTheCart() {
+    itemsInCart += 1;
+}
+
+
+
+addAnItemToTheCart();
+addAnItemToTheCart();
+// ^this line executes
+
+console.log (itemsInCart);

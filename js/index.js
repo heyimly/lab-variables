e578a20cfbbe5e40ad9aafb19ea61a37d2b7f88c
Lyndon Joseph <lyndon@Lyndons-MacBook-Pro.local>
2019-11-13 13:21:31 -0500

    oh hey gang

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 // var or const 
 let itemsInCart =0;
 
-function addAnItemToTheCart() {
-    itemsInCart += 1;
+function addAnItemToTheCart(qty) { 
+  //inside function
+
+    itemsInCart +=  qty;
+    console.log(`Add ${qty} item(s), There are now: 
+    ${itemsInCart}.`);
 }
 
 
 
-addAnItemToTheCart();
-addAnItemToTheCart();
+addAnItemToTheCart(1);
+addAnItemToTheCart(2);
 // ^this line executes
-
+//ouutside fuction
 console.log (itemsInCart);
